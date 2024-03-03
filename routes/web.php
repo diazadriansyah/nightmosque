d58476119d829923cc655b07f1bdec73fe508f2d
diazadriansyah <diazadr.dev@gmail.com>
2024-03-03 08:20:55 +0700

    update video 5

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('logout-user', function () {
+    Auth::logout();
+    return redirect('/');
+})->name('logout-user');
+
 Route::get('/', function () {
-    return view('welcome');
-});
+        return view('welcome');
+    });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
